30d62473836525e4905f391afe5f922046992a92
debabratamalakar88 <debabratamalakar88@gmail.com>
2024-07-25 13:23:15 +0530

    cc

--- ReactElectrix/src/components/Fetchitems.jsx
+++ ReactElectrix/src/components/Fetchitems.jsx
@@ -4,7 +4,7 @@ import { itemsActions } from "../store/itemsSlice";
 import { fetchStatusActions } from "../store/fetchStatusSlice";
 
 const FetchItems = () => {
-  const fetchStatus = useSelector((store) => store.fetchStatus);
+  /*const fetchStatus = useSelector((store) => store.fetchStatus);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -17,15 +17,15 @@ const FetchItems = () => {
     fetch("http://localhost:8080/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
-        //dispatch(fetchStatusActions.markFetchDone());
-        //dispatch(fetchStatusActions.markFetchingFinished());
-        //dispatch(itemsActions.addInitialItems(items[0]));
+        dispatch(fetchStatusActions.markFetchDone());
+        dispatch(fetchStatusActions.markFetchingFinished());
+        dispatch(itemsActions.addInitialItems(items[0]));
       });
 
     return () => {
       controller.abort();
     };
-  }, [fetchStatus]);
+  }, [fetchStatus]);*/
 
   return <></>;
 };
